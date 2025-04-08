0f3fc792fea0a07dd1c7080c09542a2e4da53d29
Ajay Gopi <ajaynaidu96@outlook.com>
2025-04-07 20:51:52 -0400

    A4

--- assignments/src/preprocessor.py
+++ assignments/src/preprocessor.py
@@ -1,5 +1,6 @@
 from collections import Counter
 import numpy as np
+import pandas as pd
 
 def Reweighing(X, Y, A):
     # X: independent variables (2-d pd.DataFrame)
@@ -8,6 +9,27 @@ def Reweighing(X, Y, A):
     # Return: sample_weight, an array of float weight for every data point
     #         sample_weight(a,y) = P(y)*P(a)/P(a,y)
     # Write your code below:
+
+    X = pd.DataFrame(X)
+    data = X[A].copy()
+    data['Y'] = Y
+
+    total = len(data)
+    p_y = data['Y'].value_counts(normalize=True).to_dict()
+    p_a = data[A].value_counts(normalize=True).to_dict()
+    p_ay = data.groupby(A + ['Y']).size() / total
+
+    sample_weight = []
+    for _, row in data.iterrows():
+        a_key = tuple(row[a] for a in A)
+        y_val = row['Y']
+        try:
+            weight = (p_y[y_val] * p_a[a_key]) / p_ay.loc[(*a_key, y_val)]
+        except KeyError:
+            weight = 0
+        sample_weight.append(weight)
+
+    sample_weight = np.array(sample_weight)
 
 
     # Rescale the sum of sample weights to len(y) before returning it
